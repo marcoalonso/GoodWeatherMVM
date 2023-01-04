8c2fb92ae26c1838851523ada5703557bcb9bea1
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-01-04 11:43:59 -0600

    Displaying hard-codded data

--- GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
+++ GoodWeatherMVVM/Controllers/WeatherListTableViewController.swift
@@ -12,9 +12,7 @@ class WeatherListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         setupUI()
-        
         
     }
     
@@ -32,6 +30,24 @@ class WeatherListTableViewController: UITableViewController {
         
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        cell.textLabel?.text = "Morelia"
+        cell.detailTextLabel?.text = "13° C"
+        return cell
+    }
 
 
 }
---
+++ GoodWeatherMVVM/CustomCells/WeatherCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCell.swift
+//  GoodWeatherMVVM
+//
+//  Created by Marco Alonso Rodriguez on 04/01/23.
+//
+
+import Foundation
+
+import UIKit
+
+class WeatherCell : UITableViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityTemperatureLabel: UILabel!
+}
