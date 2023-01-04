94cd96da9b886dc04155db67e587ebe4db96c322
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-01-04 11:07:18 -0600

    Setting up View Controllers

---
+++ GoodWeatherMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeatherMVVM
+//
+//  Created by Marco Alonso Rodriguez on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
