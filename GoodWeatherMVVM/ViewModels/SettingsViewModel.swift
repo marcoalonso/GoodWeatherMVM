b5725c13cb6759c8b32b79450d2961d44424bfab
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-01-05 09:54:04 -0600

    48 Implementing Settings View Model

---
+++ GoodWeatherMVVM/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsViewModel.swift
+//  GoodWeatherMVVM
+//
+//  Created by Marco Alonso Rodriguez on 05/01/23.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+    case celcius = "metric"
+    case fahrenheit = "imperial"
+}
+
+extension Unit {
+    var displayName: String {
+        get {
+            switch(self) {
+            case .celcius:
+                return "Celcius"
+            case .fahrenheit:
+                return "Fahrenheit"
+            }
+        }
+    }
+}
+
+class SettingsViewModel {
+    
+    let units = Unit.allCases
+    
+    var selectedUnit : Unit {
+        get {
+            let userDefault = UserDefaults.standard
+            var unitValue = ""
+            if let value = userDefault.value(forKey: "unit") as? String {
+                unitValue = value
+            }
+            return Unit(rawValue: unitValue)!
+        }
+        set {
+            let userDefault = UserDefaults.standard
+            userDefault.set(newValue.rawValue, forKey: "unit")
+        }
+    }
+}
